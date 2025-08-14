3036159a11a7013833d376ec3eef68501fd63f30
TOK ZUAN YOU <TOK ZUAN YOU@LAPTOP-PUEAQ0A9>
2025-08-14 13:26:15 +0800

    Create Rental Class with rental number and rental number generator

---
+++ src/car/rental/system/Registration.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package car.rental.system;
+
+/**
+ *
+ * @author TOK ZUAN YOU
+ */
+public class Registration {
+    private int rentalNo;
+    private int nextRentNo = 1001;
+
+    //Constructor
+
+    public Registration() {
+    }
+    
+    public Registration(int rentalNo) {
+        this.rentalNo = nextRentNo;
+        nextRentNo++;
+    }
+
+    public int getRentalNo() {
+        return rentalNo;
+    }
+    
+    
+    
+}
+
