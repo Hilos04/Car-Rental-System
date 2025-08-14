1b373a52a4298c38d25f08dea756f4cf3df71483
TOK ZUAN YOU <TOK ZUAN YOU@LAPTOP-PUEAQ0A9>
2025-08-14 13:48:53 +0800

    Excluded one constructor and refactored name from registration to rental

---
+++ src/car/rental/system/Rental.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package car.rental.system;
+
+/**
+ *
+ * @author TOK ZUAN YOU
+ */
+public class Rental {
+    private int rentalNo;
+    private int nextRentNo = 1001;
+
+    //Constructor
+    public Rental() {
+        this.rentalNo = nextRentNo;
+        nextRentNo++;
+    }
+
+    public int getRentalNo() {
+        return rentalNo;
+    }
+    
+    public String toString(){
+        return String.format("%-6s\n", rentalNo);
+    }    
+}
+
+
