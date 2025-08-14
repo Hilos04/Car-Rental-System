40cf43991b3f52727b365ee1f46fb1cdac903657
TOK ZUAN YOU <TOK ZUAN YOU@LAPTOP-PUEAQ0A9>
2025-08-14 13:44:50 +0800

    Created Test class for rental and customer registration class

---
+++ src/car/rental/system/ZYTesting.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package car.rental.system;
+
+/**
+ *
+ * @author TOK ZUAN YOU
+ */
+public class ZYTesting {
+    
+    public static void main(String[] args){
+        Registration reg = new Registration();
+        
+        System.out.println(reg.toString());
+    }
+}
