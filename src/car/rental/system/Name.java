49e98379e998f0a79d836c5d098685510c29908d
TOK ZUAN YOU <TOK ZUAN YOU@LAPTOP-PUEAQ0A9>
2025-08-18 11:25:44 +0800

    Created Name class

---
+++ src/car/rental/system/Name.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package car.rental.system;
+
+/**
+ *
+ * @author TOK ZUAN YOU
+ */
+public class Name {
+    private String firstName;
+    private String lastName;
+
+    public Name() {
+    }
+
+    public Name(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("First Name: " + firstName + "Last Name: " + lastName);
+    }
+    
+}
